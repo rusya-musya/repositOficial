e75720556c9d1c30e1ef964a3d6e9de5b5a1caa4
rusya-musya <55921075+rusya-musya@users.noreply.github.com>
2019-09-29 00:56:01 +0700

    join2 version2
    was updated join2

--- join2.php
+++ join2.php
@@ -4,4 +4,8 @@ echo "mr.Sbrodov git join2";
 echo "mr.Sbrodov git join2";
 echo "mr.Sbrodov git join2";
 echo "mr.Sbrodov git join2";
+echo "mr.Sbrodov git join3";
+echo "mr.Sbrodov git join3";
+echo "mr.Sbrodov git join3";
+echo "mr.Sbrodov git join3";
 ?>
